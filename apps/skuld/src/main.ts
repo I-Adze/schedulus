a04c8ed357728ba7007c31ffd2fae5838969e5a6
I-Adze <adam.ward1202@gmail.com>
2023-02-01 22:46:25 +0000

    Adds initial test connection to surrealdb

--- apps/skuld/src/main.ts
+++ apps/skuld/src/main.ts
@@ -2,6 +2,30 @@ import express from 'express';
 import * as path from 'path';
 import { Task } from '@schedulus/shared';
 
+import Surreal from 'surrealdb.js';
+
+const db = new Surreal('http://127.0.0.1:8000/rpc');
+
+(async() => {
+	try {
+		// Signin as a namespace, database, or root user
+		await db.signin({
+			user: 'root',
+			pass: 'root',
+		});
+
+    await db.use('test', 'test');
+
+    // Select all people records
+		let people = await db.select("person");
+
+    console.log(people)
+
+	} catch (e) {
+		console.error('ERROR', e);
+	}
+})()
+
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
--- libs/shared/src/lib/task.ts
+++ libs/shared/src/lib/task.ts
@@ -1,3 +1,7 @@
+export type DateString = string;
+
 export interface Task {
   id: string;
+  name: string;
+  completed: DateString[]
 }
